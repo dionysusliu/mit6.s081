4be5ca0fac121441a74aeb99f4da3587139ebc72
leo <chuangliu0815@gmail.com>
2025-06-10 07:52:09 +0800

    finish lab1 sleep

---
+++ user/pingpong.c
@@ -0,0 +1,15 @@
+#include "kernel/types.h"
+#include "user/user.h"
+
+int main(int argc, char *argv[])
+{ 
+    // create a pipe
+
+    // fork a child
+
+    // child sent bytes to pipe
+
+    // parent recv bytes from pipe
+
+    // delete the pipe
+}
