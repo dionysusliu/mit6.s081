b5165e33f888f80d86519b71fc63c9b95a22dee8
leo <chuangliu0815@gmail.com>
2025-06-10 21:36:00 +0800

    finish pingpong

--- user/pingpong.c
+++ user/pingpong.c
@@ -4,12 +4,27 @@
 int main(int argc, char *argv[])
 { 
     // create a pipe
+    int p[2];
+    char buf;
 
-    // fork a child
+    if (pipe(p) < 0) {
+        fprintf(2, "piped failed\n");
+        exit(1);
+    }
 
-    // child sent bytes to pipe
+    if (fork() == 0) { // child
+        if (read(p[0], &buf, 1) < 0) {
+            fprintf(2, "child read failed\n");
+        }
+        printf("%d: received ping\n", getpid());
+        write(p[1], "x", 1);
+    } else { // parent
+        write(p[1], "p", 1);
+        if (read(p[0], &buf, 1) < 0) {
+            fprintf(2, "parent read failed\n");
+        }
+        printf("%d: received pong\n", getpid());
+    }
 
-    // parent recv bytes from pipe
-
-    // delete the pipe
+    return 0;
 }
