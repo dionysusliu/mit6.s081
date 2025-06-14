73df315a862097f2b007824b9558e605b8260e40
leo <chuangliu0815@gmail.com>
2025-06-14 10:41:57 +0800

    complete lab1-find

--- user/find.c
+++ user/find.c
@@ -2,6 +2,7 @@
 #include "user/user.h"
 #include "kernel/fs.h"
 #include "kernel/stat.h"
+#include "kernel/fcntl.h"
 
 char*
 fmtname(char *path) {
@@ -14,42 +15,97 @@ fmtname(char *path) {
     for (p=path+strlen(path); p >= path && *p != '/'; p--)
         ;
     p++;
-    // return blank-padded name
+    // return zero-padded name
     if (strlen(p) >= DIRSIZ)
         return p;
     memmove(buf, p, strlen(p));
-    memset(buf+strlen(p), ' ', DIRSIZ-strlen(p));
+    memset(buf+strlen(p), '\0', DIRSIZ-strlen(p));
     return buf;
 }
 
-int name_matched(char *this, char *target) {
+int name_matched(char *this, const char *target) {
+    // printf("        formatted name is %s\n", fmtname(this));
+    // printf("        target  is %s\n", target);  
+    // printf(" compare result is %d\n", strcmp(fmtname(this), target));
     return strcmp(fmtname(this), target) == 0;
 }
 
-void find_helper(char *target, char *curr_path) {
+void find_helper(const char *target, char *curr_path) {
     // @target: name to be matched
     // @curr_path: current entry to be matched
-    
-}
 
-void
-find(char *target) {
-    char buf[512], *p, curr_dir[512]=".";
+    char buf[512], *p;
     int fd;
     struct dirent de;
     struct stat st;
 
-    // start from .
-    if ((fd=open(curr_dir, 0)) < 0) {
-        fprintf(2, "find: cannot open %s\n", curr_dir);
+    // printf("@@ current path: %s\n", curr_path);
+
+    if ((fd=open(curr_path, O_RDONLY)) < 0) {
+        fprintf(2, "find: cannot open %s\n", curr_path);
         return;
     }
 
     if (fstat(fd, &st) < 0) {
-        fprintf(2, "find: cannot stat %s\n", curr_dir);
+        fprintf(2, "find: cannot stat %s\n", curr_path);
         close(fd);
         return;
     }
 
-    
+    switch(st.type) {
+    case T_FILE:
+    case T_DEVICE:
+        if (name_matched(curr_path, target)) {
+            printf("%s\n", curr_path);
+        }
+        break;
+    case T_DIR: 
+        // match this entry
+        if (name_matched(curr_path, target)) {
+            printf("%s\n", curr_path);
+        }
+
+        // match sub-entries recursively
+        strcpy(buf, curr_path);
+        p = buf + strlen(buf); // pos next to the last char of string
+        *p++ = '/';
+
+        while (read(fd, &de, sizeof(de)) == sizeof(de)) {
+            if (de.inum == 0) 
+                continue;
+            if (strcmp(de.name, ".") == 0 || strcmp(de.name, "..") == 0)
+                continue;
+            // entry length check
+            if (strlen(curr_path) + 1 + strlen(de.name) + 1 > sizeof buf) {
+                printf("find: path too long\n");
+                continue;
+            }
+            memmove(p, de.name, DIRSIZ);
+            p[DIRSIZ] = 0;
+            find_helper(target, buf);
+        }
+        break;
+    }
+
+    close(fd);
 }
+
+void
+find(char *curr_path , char *target) {
+    find_helper(target, curr_path);
+    // printf("exit find helper\n");
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        // printf("Usage: find <target>");
+        exit(1);
+    }
+
+    char *curr_dir, *target;
+    curr_dir = argv[1];
+    target = argv[2];
+    find(curr_dir, target);    
+    // printf("exit find\n");
+    exit(0);
+} 
